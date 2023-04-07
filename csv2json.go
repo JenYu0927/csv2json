b62b6bb4ded435e07baefdd2daf44d3a098ab69f
Taylor Jiang <taylor.jiang@qctrd.com>
2023-04-07 15:11:46 +0800

    Change to chatGPT version

--- csv2json.go
+++ csv2json.go
@@ -1,195 +1,132 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-
-	"encoding/csv"
-	"encoding/json"
-
-	"github.com/urfave/cli"
+    "encoding/csv"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "os"
+    "path/filepath"
+    "strings"
 )
 
+// DataFormat represents a supported data format.
+type DataFormat int
+
+const (
+    Unknown DataFormat = iota
+    CSV
+    // Add additional data formats here...
+)
+
+// Employee represents an employee record.
 type Employee struct {
-	ID          int
-	FirstName   string `json:"FirstName"`
-	LastName    string `json:"LastName"`
-	Email       string `json:"Email"`
-	Description string `json:"Description"`
-	Role        string `json:"Role"`
-	Phone       string `json:"Phone"`
+    ID          string `json:"id"`
+    FirstName   string `json:"first_name"`
+    LastName    string `json:"last_name"`
+    Email       string `json:"email"`
+    Description string `json:"description"`
+    Role        string `json:"role"`
+    Phone       string `json:"phone"`
 }
 
-var outputFormatFunctions = map[string]func([]Employee, string) error{"json": writeToJsonFile} // determine by variable outputFormat
-type Reader interface {
-	read() ([]Employee, error)
+// Converter is responsible for converting data from one format to another.
+type Converter interface {
+    Convert(reader io.Reader) ([]Employee, error)
 }
 
-type Writer interface {
-	write() error
+// CSVConverter is a Converter that can read data from CSV files.
+type CSVConverter struct{}
+
+// Convert reads data from a CSV file and returns a slice of Employee records.
+func (c CSVConverter) Convert(reader io.Reader) ([]Employee, error) {
+    csvReader := csv.NewReader(reader)
+    var employees []Employee
+
+    for {
+        line, err := csvReader.Read()
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            return nil, err
+        }
+
+        employee := Employee{
+            ID:          line[0],
+            FirstName:   line[1],
+            LastName:    line[2],
+            Email:       line[3],
+            Description: line[4],
+            Role:        line[5],
+            Phone:       line[6],
+        }
+
+        employees = append(employees, employee)
+    }
+
+    return employees, nil
 }
 
-type csvReader struct {
-	fileName string
+// JSONConverter is a Converter that can write data to JSON files.
+type JSONConverter struct{}
+
+// Convert writes data to a JSON file and returns an error if any.
+func (c JSONConverter) Convert(writer io.Writer, employees []Employee) error {
+    jsonData, err := json.MarshalIndent(employees, "", "    ")
+    if err != nil {
+        return err
+    }
+
+    _, err = writer.Write(jsonData)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
-type jsonWriter struct {
-	filename string
+// GetConverter returns a Converter that can handle the specified data format.
+func GetConverter(format DataFormat) (Converter, error) {
+    switch format {
+    case CSV:
+        return CSVConverter{}, nil
+    // Add additional data formats here...
+    default:
+        return nil, errors.New("unsupported data format")
+    }
 }
 
-func (r csvReader) read() ([]Employee, error) {
-	csvFile, err := os.Open(r.fileName)
-	defer csvFile.Close()
-	if err != nil {
-		return []Employee{}, err
-	}
-
-	csvReader := csv.NewReader(csvFile)
-	lines, err := csvReader.ReadAll()
-	if err != nil {
-		return []Employee{}, err
-	}
-
-	var employees []Employee = []Employee{}
-	for index, line := range lines {
-		if index != 0 { // skip first line which is the title of below lines
-			id, err := strconv.Atoi(line[0])
-			if err != nil {
-				return nil, err
-			}
-			employee := Employee{ID: id, FirstName: line[1], LastName: line[2],
-				Email: line[3], Description: line[4], Role: line[5], Phone: line[6]}
-			employees = append(employees, employee)
-		}
-	}
-
-	return employees, nil
+// DetectDataFormat returns the data format of the specified file based on its file extension.
+func DetectDataFormat(filename string) (DataFormat, error) {
+    ext := strings.ToLower(filepath.Ext(filename))
+    switch ext {
+    case ".csv":
+        return CSV, nil
+    // Add additional file extensions here...
+    default:
+        return Unknown, errors.New("unknown file extension")
+    }
 }
-
-func readCSV(csvFileName string) ([]Employee, error) {
-	csvFile, err := os.Open(csvFileName)
-	defer csvFile.Close()
-	if err != nil {
-		return []Employee{}, err
-	}
-
-	csvReader := csv.NewReader(csvFile)
-	lines, err := csvReader.ReadAll()
-	if err != nil {
-		return []Employee{}, err
-	}
-
-	var employees []Employee = []Employee{}
-	for index, line := range lines {
-		if index != 0 { // skip first line which is the title of below lines
-			id, err := strconv.Atoi(line[0])
-			if err != nil {
-				return nil, err
-			}
-			employee := Employee{ID: id, FirstName: line[1], LastName: line[2],
-				Email: line[3], Description: line[4], Role: line[5], Phone: line[6]}
-			employees = append(employees, employee)
-		}
-	}
-
-	return employees, nil
-}
-
-func writeToJsonFile(employees []Employee, outputFileName string) error {
-	employeesByte, err := json.Marshal(employees)
-	if err != nil {
-		return err
-	}
-	//fmt.Println(string(employeesByte))
-
-	outputFile, err := os.OpenFile(outputFileName, os.O_WRONLY|os.O_CREATE, 0664)
-	if err != nil {
-		return err
-	}
-	defer outputFile.Close()
-
-	_, err = outputFile.Write(employeesByte)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-/*
-func writeToYaml([]Employee) error {
-	return nil
-}*/
 
 func main() {
-	var outputFileName string
-	var outputFormat string
+    if len(os.Args) != 3 {
+        fmt.Printf("Usage: %s input_file output_file\n", os.Args[0])
+        os.Exit(1)
+    }
 
-	//outputFormatFunctions := map[string]func([]Employee, string) error{"json": writeToJsonFile} // determine by variable outputFormat
+    inputFile := os.Args[1]
+    outputFile := os.Args[2]
 
-	app := cli.NewApp()
-	app.Name = "csv2json"
-	app.Usage = "convert csv file to json file"
+    format, err := DetectDataFormat(inputFile)
+    if err != nil {
+        log.Fatalf("Error detecting data format: %s", err)
+    }
 
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:        "input,i",
-			Usage:       "Specify the name of file to parse",
-			Value:       "Employees",
-			Destination: &outputFileName,
-		},
-		&cli.StringFlag{
-			Name:        "output,o",
-			Usage:       "Specify the output file name",
-			Value:       "Employees",
-			Destination: &outputFileName,
-		},
-		&cli.StringFlag{
-			Name:        "format,f",
-			Usage:       "Specify format of output file",
-			Value:       "json",
-			Destination: &outputFormat,
-		},
-	}
-	app.Action = run
+    converter, err := GetConverter(format)
+    if err != nil {
+        log.Fatalf("Error getting converter: %s", err)
+    }
 
-	//fmt.Println(os.Args)
-	app.Run(os.Args)
-}
-
-func run(c *cli.Context) error {
-	//println(outputFileName, outputFormat, outputFormatFunctions[outputFormat], reflect.TypeOf(outputFormatFunctions[outputFormat]))
-	if c.NArg() < 1 {
-		fmt.Println("Designate a file to parse")
-		return fmt.Errorf("Designate a file to parse")
-	} else if c.NArg() > 1 {
-		fmt.Println("This tool require only one parameter as target file")
-		return fmt.Errorf("This tool require only one parameter as target file")
-	} else if _, err := os.Stat(c.Args().Get(0)); os.IsNotExist(err) {
-		fmt.Println("Designated file dose not exist")
-		return err
-	} else if _, ok := outputFormatFunctions[outputFormat]; !ok { // Non supported output format
-		fmt.Println("Wrong output format")
-		return fmt.Errorf("Wrong output format")
-	} else {
-		employees, err := readCSV(c.Args().Get(0)) // read CSV into array of Employee
-		//fmt.Println(employees)
-		if err != nil {
-			fmt.Println("Parsing csv file failed. Error message:", err)
-			return err
-		} else if len(employees) == 0 {
-			fmt.Println("Can't parse any employee from target file")
-			return fmt.Errorf("Can't parse any employee from target file")
-		}
-		//fmt.Println(employees, reflect.TypeOf(employees))
-
-		err = outputFormatFunctions[outputFormat](employees, outputFileName) // call output function which in map outputFormatFunctions
-		if err != nil {
-			return err
-		}
-		fmt.Println("Convert successfully! Output File is:", outputFileName)
-	}
-
-	return nil
-}
+    inputReader, err
